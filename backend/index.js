df03e14e6d66e9525568e74475c1029dbc7fec8a
Prateek <Prateekrajgrd74@gmail.com>
2025-07-29 04:49:41 +0530

    cors policy fixed

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ app.use(cors({
     }
   },
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
 
